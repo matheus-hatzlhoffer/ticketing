96fa724d7c0c20a0a007e92e38d6b55b993d881b
Matheus Carvalho Hatzlhoffer <matheus.hatzlhoffer@gmail.com>
2021-07-24 18:10:07 -0300

    finishing the sign in and sign up route

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../utils/password';
 
 // What is need to create a User
 interface UserAttrs {
@@ -17,15 +18,35 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
   },
+);
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,55 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../utils/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must bem valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a valid password'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credencials');
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password,
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid Credencials');
+    }
+
+    // Generate Json web token
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    req.session = {
+      jkt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  },
+);
 
 export { router as signinRouter };
